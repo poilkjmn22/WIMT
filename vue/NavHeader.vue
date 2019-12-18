34928367432cee36b96b45cb68dd25bdf5102286
qi.fang <qi.fang@wenba100.com>
2019-12-18 18:58:00 +0800

    build

--- vue/NavHeader.vue
+++ vue/NavHeader.vue
@@ -1,7 +1,11 @@
 <template lang="html">
   <el-menu :default-active="activeIndex" mode="horizontal" :router="true">
     <el-menu-item :index="parseNavMenuItemIndex('list')" :route="{name: 'list'}">首页</el-menu-item>
-    <el-menu-item :index="parseNavMenuItemIndex('d3Charts')" :route="{name: 'd3Charts'}">d3 charts</el-menu-item>
+    <el-submenu index="1">
+      <template slot="title">d3可视化</template>
+      <el-menu-item :index="parseNavMenuItemIndex('d3Charts')" :route="{name: 'd3Charts'}">d3 charts</el-menu-item>
+    </el-submenu>
+
     <el-submenu index="2">
       <template slot="title"><i class="el-icon-setting"></i>设置</template>
       <el-menu-item :index="parseNavMenuItemIndex('addActivity')" :route="{name: 'addActivity'}">添加活动</el-menu-item>
